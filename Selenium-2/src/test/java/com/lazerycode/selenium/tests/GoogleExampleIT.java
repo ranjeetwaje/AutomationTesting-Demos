c69275fb3f25897836601f07cdbea9952320ae05
SaurabhDev <singhalsaurabh20@gmail.com>
2019-10-20 21:12:17 +0530

    modified

--- Selenium-2/src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
+++ Selenium-2/src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
@@ -30,13 +30,13 @@ public class GoogleExampleIT extends DriverBase {
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
 
-        googleHomePage.enterSearchTerm("Cheese")
+        googleHomePage.enterSearchTerm("DevOps")
                 .submitSearch();
 
         // Google's search is rendered dynamically with JavaScript.
         // Wait for the page to load, timeout after 10 seconds
         WebDriverWait wait = new WebDriverWait(driver, 10, 100);
-        wait.until(pageTitleStartsWith("Cheese"));
+        wait.until(pageTitleStartsWith("DevOps"));
 
         // Should see: "cheese! - Google Search"
         System.out.println("Page title is: " + driver.getTitle());
@@ -59,13 +59,13 @@ public class GoogleExampleIT extends DriverBase {
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
 
-        googleHomePage.enterSearchTerm("Milk")
+        googleHomePage.enterSearchTerm("Cloud Computing")
                 .submitSearch();
 
         // Google's search is rendered dynamically with JavaScript.
         // Wait for the page to load, timeout after 10 seconds
         WebDriverWait wait = new WebDriverWait(driver, 10, 100);
-        wait.until(pageTitleStartsWith("Milk"));
+        wait.until(pageTitleStartsWith("Cloud Computing"));
 
         // Should see: "cheese! - Google Search"
         System.out.println("Page title is: " + driver.getTitle());
